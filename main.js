3c4d0b0ea2e9269d58ab308224ddf6aaa92d80fc
Silje32 <msh070425@gmail.com>
2025-09-15 09:33:36 +0200

    Made a get request to the evolution-chain api

---
+++ main.js
@@ -0,0 +1,7 @@
+const url = "https://pokeapi.co/api/v2/evolution-chain";
+async function getData() {
+  let response = await fetch(url);
+  let data = await response.json();
+  console.log(data);
+}
+getData();
