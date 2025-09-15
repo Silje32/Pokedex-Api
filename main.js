0d497ab94a3d2987d81886ebe1d0547aacd9da73
Silje32 <msh070425@gmail.com>
2025-09-15 10:17:23 +0200

    Added the rest of my img variables and changed the name on the first one

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-const displayImg = document.querySelector("#img-evolutions");
+const displayImg1 = document.querySelector("#img-evolutions_1");
+const displayImg2 = document.querySelector("#img-evolutions_2");
+const displayImg3 = document.querySelector("#img-evolutions_3");
+
 const url = "https://pokeapi.co/api/v2/evolution-chain";
 
 // Handles API data
-async function getData() {
+async function getData(url) {
   let response = await fetch(url);
   let data = await response.json();
   console.log(data);
@@ -12,9 +15,10 @@ getData();
 // Renders element to page
 function renderImg(data) {
   data.forEach((element) => {
-    const img = document.createElement("img-evolutions");
-    img.src = element.img;
-    img.alt = element.name;
-    displayImg.appendChild(img);
+    const img1 = document.createElement("img-evolutions_1");
+    img1.src = element.img;
+    img1.alt = element.name;
+    displayImg1.appendChild(img1);
   });
 }
+renderImg(url);
