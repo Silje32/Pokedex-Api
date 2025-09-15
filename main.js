3e6db9fbe800ec187e59ff92e144fc2749ef755d
Silje32 <msh070425@gmail.com>
2025-09-15 09:54:04 +0200

    Added comments in js file and my renderImg function

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 const displayImg = document.querySelector("#img-evolutions");
-
 const url = "https://pokeapi.co/api/v2/evolution-chain";
+
+// Handles API data
 async function getData() {
   let response = await fetch(url);
   let data = await response.json();
   console.log(data);
 }
 getData();
+
+// Renders element to page
+function renderImg(data) {
+  data.forEach((element) => {
+    const img = document.createElement("img-evolutions");
+    img.src = element.img;
+    img.alt = element.name;
+    displayImg.appendChild(img);
+  });
+}
